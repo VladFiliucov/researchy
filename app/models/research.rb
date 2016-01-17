eeede02e934af8562be3da143ab48d7f52aeb0d0
VladFiliucov <filiucov@mail.ru>
2016-01-17 16:31:02 +0000

    Add presence validations to Research attributes.

--- app/models/research.rb
+++ app/models/research.rb
@@ -1,2 +1,7 @@
 class Research < ActiveRecord::Base
+  validates :title, presence: true
+  validates :category, presence: true
+  validates :author, presence: true
+  validates :description, presence: true
+  validates :body, presence: true
 end
