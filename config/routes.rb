21311856e0efc8ebadbc54f931f5d93ef2030233
VladFiliucov <filiucov@mail.ru>
2016-01-17 16:04:47 +0000

    Add Research controller.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
+  get 'research/show'
+
+  get 'research/new'
+
+  get 'research/create'
+
+  get 'research/edit'
+
+  get 'research/update'
+
+  get 'research/destroy'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
