7ab0c26980c09d4c5514147ea4a91d4adb0fb064
VladFiliucov <filiucov@mail.ru>
2016-01-23 09:38:08 +0000

    install devise

--- Gemfile
+++ Gemfile
@@ -12,6 +12,7 @@ gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
 gem 'rails_12factor'
 gem 'bootstrap', '~> 4.0.0.alpha3'
+gem 'devise'
 
 source 'https://rails-assets.org' do
   gem 'rails-assets-tether', '>= 1.1.0'
@@ -26,6 +27,7 @@ group :development, :test do
   gem 'factory_girl_rails'
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
+  gem 'shoulda-matchers'
 end
 
 group :test do
---
+++ spec/features/sign_in_spec.rb
@@ -0,0 +1,26 @@
+require "rails_helper"
+
+feature 'User sign in' do
+  
+  scenario "existing user sign_in" do
+    User.create!(email: "email@example.com", password: '123456789')
+
+    visit new_user_session_path
+    fill_in "Email", with: "email@example.com"
+    fill_in "Password", with: "123456789"
+    click_on "Sign in"
+
+    expect(page).to have_content 'Signed in successfully'
+    expect(current_path).to eq(root_path)
+  end
+
+  scenario "non existend user can't sign in" do
+    visit new_user_session_path
+    fill_in "Email", with: "email@example.com"
+    fill_in "Password", with: "123456789"
+    click_on "Sign in"
+
+    expect(page).to have_content "Invalid email or password"
+    expect(current_path).to eq(new_user_session_path)
+  end
+end
