90c0492790b595585680cb3e2185077bc4041f20
VladFiliucov <filiucov@mail.ru>
2016-01-26 22:05:44 +0000

    Fix mailer.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -45,7 +45,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
-    port:                 587,
+    port:                 25,
     domain:               'researchy.herokuapp.com',
     user_name:            ENV['GMAIL_USERNAME'],
     password:             ENV['GMAIL_PASSWORD'],
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,9 +77,9 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 # config.action_mailer.default_url_options = { :host => 'researchy.herokuapp.com' } 
-#  config.action_mailer.delivery_method = :sendmail
-#  config.action_mailer.perform_deliveries = true
-#  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
 #  config.action_mailer.smtp_settings = {
 #    address:              'smtp.gmail.com',
 #    port:                 587,
@@ -88,6 +88,15 @@ Rails.application.configure do
 #    password:             ENV['GMAIL_PASSWORD'],
 #    authentication:       'plain',
 #    enable_starttls_auto: true  }
+ActionMailer::Base.smtp_settings = {
+  :address        => 'smtp.sendgrid.net',
+  :port           => '587',
+  :authentication => :plain,
+  :user_name      => ENV['SENDGRID_USERNAME'],
+  :password       => ENV['SENDGRID_PASSWORD'],
+  :domain         => 'heroku.com',
+  :enable_starttls_auto => true
+}
 end
 
 #config.action_mailer.smtp_settings = {
