d6d7f10336a4b7b7ae9e1c0f11b2a16fa6f83532
VladFiliucov <filiucov@mail.ru>
2016-02-07 22:25:13 +0000

    fixing sidekiq production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,7 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  ENV["REDISTOGO_URL"] = 'redis://username:password@my.host:6389'
+  # ENV["REDISTOGO_URL"] = 'redis://username:password@my.host:6389'
   # Code is not reloaded between requests.
   config.cache_classes = true
 
